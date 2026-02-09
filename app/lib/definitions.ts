b77dd5e31ffd30d193cb4663fa9f271cf5e8bf0e
Gabrie7ven <gabriel.oficial@yahoo.com>
2026-02-08 21:51:25 -0300

    adjusting /seed route

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -9,80 +9,86 @@ export type User = {
   password: string;
 };
 
-export type Customer = {
+export type Participant = {
   id: string;
   name: string;
   email: string;
+  birth_date: string;
   image_url: string;
+  team_id: string;
 };
 
-export type Invoice = {
+export type Team = {
   id: string;
-  customer_id: string;
-  amount: number;
+  name: string;
+}
+export type Appointment = {
+  id: string;
+  team_id: string;
   date: string;
+  details: string;
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
-  status: 'pending' | 'paid';
+  status: 'pending' | 'completed';
 };
 
-export type Revenue = {
-  month: string;
-  revenue: number;
-};
+// export type Revenue = {
+//   month: string;
+//   revenue: number;
+// };
 
-export type LatestInvoice = {
-  id: string;
-  name: string;
-  image_url: string;
-  email: string;
-  amount: string;
-};
+// export type LatestInvoice = {
+//   id: string;
+//   name: string;
+//   image_url: string;
+//   email: string;
+//   amount: string;
+// };
 
-// The database returns a number for amount, but we later format it to a string with the formatCurrency function
-export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
-  amount: number;
-};
+// // The database returns a number for amount, but we later format it to a string with the formatCurrency function
+// export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
+//   amount: number;
+// };
 
-export type InvoicesTable = {
-  id: string;
-  customer_id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  date: string;
-  amount: number;
-  status: 'pending' | 'paid';
-};
+// export type InvoicesTable = {
+//   id: string;
+//   customer_id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+//   date: string;
+//   amount: number;
+//   status: 'pending' | 'paid';
+// };
 
-export type CustomersTableType = {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  total_invoices: number;
-  total_pending: number;
-  total_paid: number;
-};
+// export type CustomersTableType = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+//   total_invoices: number;
+//   total_pending: number;
+//   total_paid: number;
+// };
 
-export type FormattedCustomersTable = {
-  id: string;
-  name: string;
-  email: string;
-  image_url: string;
-  total_invoices: number;
-  total_pending: string;
-  total_paid: string;
-};
+// export type FormattedCustomersTable = {
+//   id: string;
+//   name: string;
+//   email: string;
+//   image_url: string;
+//   total_invoices: number;
+//   total_pending: string;
+//   total_paid: string;
+// };
 
-export type CustomerField = {
-  id: string;
-  name: string;
-};
+// export type CustomerField = {
+//   id: string;
+//   name: string;
+// };
 
-export type InvoiceForm = {
-  id: string;
-  customer_id: string;
-  amount: number;
-  status: 'pending' | 'paid';
-};
+// export type InvoiceForm = {
+//   id: string;
+//   customer_id: string;
+//   amount: number;
+//   status: 'pending' | 'paid';
+// };
